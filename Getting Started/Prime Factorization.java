8c28a705b3537d83c145b7ca184e4207789a46eb
Archit Aggarwal <archit.aggarwal023@gmail.com>
2021-08-24 18:04:06 +0530

    Create Prime Factorization.java

---
+++ Getting Started/Prime Factorization.java
@@ -0,0 +1,31 @@
+import java.util.*;
+
+public class Main{
+
+public static void main(String[] args) {
+    Scanner scn = new Scanner(System.in);
+    int n = scn.nextInt();
+    
+    // Approach 1
+//     for(int factor = 2; factor <= n; factor++)
+//     {
+//         while(n % factor == 0)
+//         {
+//             System.out.print(factor + " ");
+//             n = n / factor;
+//         }
+//     }
+  
+    // Approach 2: Optimization
+    for(int factor = 2; factor * factor <= n; factor++)
+    {
+        while(n % factor == 0)
+        {
+            System.out.print(factor + " ");
+            n = n / factor;
+        }
+    }
+    
+    if(n != 1) System.out.print(n);
+ }
+}
