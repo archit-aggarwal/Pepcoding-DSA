a5c9722acf59f93dc28b34a8439971cd0799de57
Archit Aggarwal <archit.aggarwal023@gmail.com>
2021-08-24 18:25:57 +0530

    Create 17.java

---
+++ Patterns/17.java
@@ -0,0 +1,34 @@
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        int n = scn.nextInt();
+        int stars = 1, spaces = n/2;
+        for(int i=1; i<=n; i++)
+        {
+            for(int j=1; j<=spaces; j++)
+                System.out.print("	");
+            for(int j=1; j<=stars; j++)
+            {
+                if(i == n/2 + 1 || j >= stars/2 + 1)
+                    System.out.print("*	");
+                else
+                    System.out.print("	");
+            }
+            
+            System.out.println();
+            if(i <= n/2)
+            {
+                stars += 2;
+                spaces--;
+            }
+            else
+            {
+                stars -= 2;
+                spaces++;
+            }
+        }
+    }
+}
