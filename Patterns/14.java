20421258286f5d3f25b4e88ea76df721a0211176
Archit Aggarwal <archit.aggarwal023@gmail.com>
2021-08-24 18:24:29 +0530

    Create 14.java

---
+++ Patterns/14.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+
+        // write ur code here
+        int n = scn.nextInt();
+        
+        for(int i=1; i<=10; i++)
+        {
+            System.out.println(n + " * " + i + " = " + n * i);
+        }
+    }
+}
