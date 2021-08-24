18cafc24b2f9a60cfbf57076f37b08552faca82d
Archit Aggarwal <archit.aggarwal023@gmail.com>
2021-08-24 18:08:48 +0530

    Create Getting Started.java

---
+++ Getting Started/Getting Started.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // Write your code here
+        
+     System.out.println("*****");
+     System.out.println("   *");
+     System.out.println("  *");
+     System.out.println(" *");
+     System.out.println("*****");
+    }
+}
