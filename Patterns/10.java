d2a898bf630d1c7cd6322c9741afb9b68e5932d1
Archit Aggarwal <archit.aggarwal023@gmail.com>
2021-08-24 18:21:09 +0530

    Update 10.java

--- Patterns/10.java
+++ Patterns/10.java
@@ -10,9 +10,13 @@ public static void main(String[] args) {
     for(int i=1; i<=n; i++)
     {
         for(int j=1; j<=spaces; j++)
-            System.out.print("\t");
+            System.out.print("	");
         for(int j=1; j<=stars; j++)
-            System.out.print("*\t");
+        {
+            if(j ==1 || j == stars)
+                System.out.print("*	");
+            else System.out.print("	");
+        }
         
         System.out.println();
         if(i <= n/2)
