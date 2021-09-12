cab4c03dda0336f22546cfadc5a375fd717b2b07
Archit Aggarwal <archit.aggarwal023@gmail.com>
2021-09-12 23:16:52 +0530

    Update GoldMine.java

--- Dynamic Programming/GoldMine.java
+++ Dynamic Programming/GoldMine.java
@@ -47,5 +47,31 @@ public class Main {
         dp[r][c] = ans;
         return ans;
     }
+    
+    public static int maxGoldTab(int[][] arr){
+        int n = arr.length, m = arr[0].length;
+        int[][] dp = new int[n][m];
+        
+        for(int j=m-1; j>=0; j--)
+        {
+            for(int i=0; i<n; i++)
+            {
+                if(j == m-1) { 
+                    dp[i][j] = arr[i][j];
+                } else {
+                    int up = (i - 1 >= 0) ? dp[i - 1][j + 1] : 0;
+                    int right = dp[i][j + 1];
+                    int down = (i + 1 < n) ? dp[i + 1][j + 1] : 0;
+                    dp[i][j] = arr[i][j] + Math.max(up, Math.max(right, down));
+                }
+            }
+        }
+        
+        int ans = 0;
+        for(int i=0; i<n; i++)
+            ans = Math.max(ans, dp[i][0]);
+        
+        return ans;
+    }
 
 }
