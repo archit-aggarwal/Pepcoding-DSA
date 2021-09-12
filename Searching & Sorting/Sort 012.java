69656a166134a2e88f4fd34075a150bd75ddb853
Archit Aggarwal <archit.aggarwal023@gmail.com>
2021-09-12 12:55:58 +0530

    Create Sort 012.java

---
+++ Searching & Sorting/Sort 012.java
@@ -0,0 +1,51 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+  public static void sort012(int[] arr){
+    int i = 0, j = 0, k = arr.length - 1;
+    while(i <= k)
+    {
+        if(arr[i] == 0)
+        {
+            swap(arr, i, j);
+            i++; j++;
+        }
+        else if(arr[i] == 1) 
+        {
+            i++;
+        }
+        else // arr[i] == 2
+        {
+            swap(arr, i, k);
+            k--;
+        }
+    }
+  }
+
+  // used for swapping ith and jth elements of array
+  public static void swap(int[] arr, int i, int j) {
+    System.out.println("Swapping index " + i + " and index " + j);
+    int temp = arr[i];
+    arr[i] = arr[j];
+    arr[j] = temp;
+  }
+
+  public static void print(int[] arr){
+    for(int i = 0 ; i < arr.length; i++){
+      System.out.println(arr[i]);
+    }
+  }
+  public static void main(String[] args) throws Exception {
+    Scanner scn = new Scanner(System.in);
+    int n = scn.nextInt();
+    int[] arr = new int[n];
+    for(int i = 0 ;i < n; i++){
+      arr[i] = scn.nextInt();
+    }
+    sort012(arr);
+    print(arr);
+  }
+
+}
