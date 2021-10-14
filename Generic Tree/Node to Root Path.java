0ca0d83fd6989621db38706e4a955a0579b68fbc
Archit Aggarwal <archit.aggarwal023@gmail.com>
2021-10-14 21:37:35 +0530

    Create Node to Root Path.java

---
+++ Generic Tree/Node to Root Path.java
@@ -0,0 +1,37 @@
+public static ArrayList<Integer> nodeToRootPath(Node node, int data) {
+    if (node.data == data) {
+      ArrayList<Integer> path = new ArrayList<>();
+      path.add(node.data);
+      return path;
+    }
+
+    for (Node child : node.children) {
+      ArrayList<Integer> ptc = nodeToRootPath(child, data);
+      if (ptc.size() > 0) {
+        ptc.add(node.data);
+        return ptc;
+      }
+    }
+
+    return new ArrayList<>();
+  }
+
+  public static int lca(Node node, int d1, int d2) {
+    ArrayList<Integer> a1 = nodeToRootPath(node, d1);
+    ArrayList<Integer> a2 = nodeToRootPath(node, d2);
+    
+    int LCA = -1;
+    int i = a1.size() - 1, j = a2.size() - 1;
+    
+    while(i >= 0 && j>= 0){
+        
+        if(a1.get(i) != a2.get(j)){
+            break;
+        }
+        
+        LCA = a1.get(i);
+        i--; j--;
+    }
+    
+    return LCA;
+  }
