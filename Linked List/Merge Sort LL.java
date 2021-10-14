ad93624ded81cd60d4ece674c32249333716c4c2
Archit Aggarwal <archit.aggarwal023@gmail.com>
2021-10-14 19:26:32 +0530

    Update Merge Sort LL.java

--- Linked List/Merge Sort LL.java
+++ Linked List/Merge Sort LL.java
@@ -1,3 +1,39 @@
+public static LinkedList mergeTwoSortedLists(LinkedList l1, LinkedList l2) {
+            LinkedList l3 = new LinkedList();
+            
+            Node p1 = l1.head, p2 = l2.head;
+            
+            while(p1 != null && p2 != null){
+                if(p1.data < p2.data){
+                    
+                    int val = p1.data;
+                    l3.addLast(val);
+                    p1 = p1.next;
+                    
+                } else {
+
+                    int val = p2.data;
+                    l3.addLast(val);
+                    p2 = p2.next;
+                    
+                }
+            }
+            
+            while(p1 != null){
+                int val = p1.data;
+                l3.addLast(val);
+                p1 = p1.next;                
+            }
+            
+            while(p2 != null){
+                int val = p2.data;
+                l3.addLast(val);
+                p2 = p2.next;                
+            }
+            
+            return l3;
+        }
+
 public static LinkedList mergeSort(Node head, Node tail)
     {
         if(head == null){
