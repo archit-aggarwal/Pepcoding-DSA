536a7b17bb0cbc095ecc00937ed4237bf49844f9
Archit Aggarwal <archit.aggarwal023@gmail.com>
2021-10-16 12:23:23 +0530

    Update Node to Root Path.java

--- Generic Tree/Node to Root Path.java
+++ Generic Tree/Node to Root Path.java
@@ -36,3 +36,19 @@ public static ArrayList<Integer> nodeToRootPath(Node node, int data) {
     
     return LCA;
   }
+
+// Time & Space - O(N)
+public static int distanceBetweenNodes(Node node, int d1, int d2){
+    ArrayList<Integer> p1 = nodeToRootPath(node, d1);
+    ArrayList<Integer> p2 = nodeToRootPath(node, d2);
+
+    int i = p1.size() - 1;
+    int j = p2.size() - 1;
+
+    while(i >= 0 && j >= 0 && p1.get(i) == p2.get(j)){
+      i--;
+      j--;
+    }
+    
+    return (i + 1) + (j + 1);
+  }
