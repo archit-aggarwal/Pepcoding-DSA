a8941c45eb4d54681b52488535fd8cf701431d02
Archit Aggarwal <archit.aggarwal023@gmail.com>
2021-11-21 20:23:20 +0530

    Update Combinations.java

--- Level 2/01. Recursion & Backtracking/Combinations.java
+++ Level 2/01. Recursion & Backtracking/Combinations.java
@@ -29,3 +29,39 @@ class Solution {
         return combinations;
     }
 }
+
+// Items on Level
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+  public static void combinations(int[] boxes, int ci, int ti, int lastItemIdx){
+    // write your code here
+    if(ci == ti){
+        for(int val: boxes){
+            if(val == 0) System.out.print("-");
+            else System.out.print("i");
+        }
+        
+        System.out.println();
+    }
+    
+    // Item -> Chose box
+    for(int i=lastItemIdx + 1; i<boxes.length; i++){
+        if(boxes[i] == 0){
+            boxes[i] = ci + 1;
+            combinations(boxes, ci + 1, ti, i);
+            boxes[i] = 0;
+        }
+    }
+  }
+
+  public static void main(String[] args) throws Exception {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    int nboxes = Integer.parseInt(br.readLine());
+    int ritems = Integer.parseInt(br.readLine());
+    combinations(new int[nboxes], 0, ritems, -1);
+  }
+
+}
