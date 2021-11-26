c9d65ee781f5a1bc846e2e634faa96ebc7923b7d
Archit Aggarwal <archit.aggarwal023@gmail.com>
2021-11-26 13:20:49 +0530

    Update Print Subsequences.java

--- Level 1/05.2. Recursion All Paths/Print Subsequences.java
+++ Level 1/05.2. Recursion All Paths/Print Subsequences.java
@@ -31,3 +31,37 @@ public class Main {
     }
 
 }
+
+
+// Method 2: Using ArrayList
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        Scanner scn = new Scanner(System.in);
+        String str = scn.nextLine();
+        
+        ArrayList<Character> subSet = new ArrayList<>();
+        printSS(str, 0, subSet);
+    }
+
+    public static void printSS(String str, int idx, ArrayList<Character> subSet) {
+        if(idx == str.length()){
+            for(Character i: subSet){
+                System.out.print(i);
+            }
+            System.out.println();
+            return;
+        }
+        
+        // yes
+        subSet.add(str.charAt(idx));
+        printSS(str, idx + 1, subSet);
+        
+        subSet.remove(subSet.size() - 1);
+        printSS(str, idx + 1, subSet);
+    }
+
+}
