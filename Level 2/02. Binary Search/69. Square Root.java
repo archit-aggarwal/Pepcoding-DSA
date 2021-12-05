182a46856f675845c97ee474a9b6a341472e2e56
Archit Aggarwal <archit.aggarwal023@gmail.com>
2021-12-05 21:15:59 +0530

    Create 69. Square Root.java

---
+++ Level 2/02. Binary Search/69. Square Root.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int mySqrt(int x) {
+        if(x == 0) return 0;
+        
+        long left = 1L, right = x;
+        long floorSqrt = 1L;
+        while(left <= right){
+            long mid = left + (right - left) / 2L;
+            long square = mid * mid;
+            
+            if(square == x){
+                return (int)mid;
+            } else if(square < x){
+                floorSqrt = mid;
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+        return (int)floorSqrt;
+    }
+}
