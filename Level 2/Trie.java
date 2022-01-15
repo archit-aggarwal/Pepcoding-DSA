2524b8a5849a6e084d85d549efc1894e71a3834f
Archit Aggarwal <archit.aggarwal023@gmail.com>
2022-01-15 15:05:30 +0530

    Update Trie.java

--- Level 2/Trie.java
+++ Level 2/Trie.java
@@ -340,3 +340,81 @@ class MapSum {
         return curr.pref;
     }
 }
+
+ 
+// Leetcode 677 (Using DFS)
+class MapSum {
+    public static class Node{
+        private Node[] children = new Node[26];
+        private int val = 0;
+        
+        public Node get(char ch){
+            return children[ch - 'a'];
+        }
+        
+        public int getVal(){
+            return val;    
+        }
+        
+        public void add(char ch){
+            children[ch - 'a'] = new Node();
+        }
+        
+        public void setVal(int val){
+            this.val = val;
+        }
+        
+        public boolean contains(char ch){
+            return (children[ch - 'a'] != null);
+        }
+    }
+    
+    Node root;
+    public MapSum() {
+        root = new Node();
+    }
+    
+    public void insert(String word, int val) {
+        Node curr = root;
+        
+        for(int i=0; i<word.length(); i++){
+            char ch = word.charAt(i);
+            
+            if(curr.contains(ch) == false)
+                curr.add(ch);
+                
+            curr = curr.get(ch);
+        }
+        
+        curr.setVal(val);
+    }
+    
+    public int DFS(Node curr){
+        int ans = 0;
+        for(char ch = 'a'; ch <= 'z'; ch++){
+            if(curr.contains(ch) == true){
+                ans += DFS(curr.get(ch));
+            }
+        }
+        return ans + curr.getVal(); 
+    }
+    public int sum(String word) {
+        Node curr = root;
+        for(int i=0; i<word.length(); i++){
+            char ch = word.charAt(i);
+            
+            if(curr.contains(ch) == false)
+                return 0;
+            
+            curr = curr.get(ch);
+        }
+        return DFS(curr);
+    }
+}
+
+/**
+ * Your MapSum object will be instantiated and called as such:
+ * MapSum obj = new MapSum();
+ * obj.insert(key,val);
+ * int param_2 = obj.sum(prefix);
+ */
