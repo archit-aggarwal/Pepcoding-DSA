f5c3f0b407b998dd4d013550e0eba3bf5615459f
Archit Aggarwal <archit.aggarwal023@gmail.com>
2022-01-15 10:36:16 +0530

    Create Trie.cpp

---
+++ Level 2/Trie.cpp
@@ -0,0 +1,66 @@
+class Node
+{
+    public:
+    char data;
+    unordered_map<char,Node*> children;
+    bool terminal;
+    Node(char data)
+    {
+        this->data = data;
+        terminal = false;
+    }
+};
+class Trie {
+public:
+    Node* root;
+    Trie() { root = new Node('\0'); }
+    
+    void insert(string s) 
+    {
+        Node* temp = root;
+        for(int i=0;i < s.size();i++)
+        {
+            if(temp->children.find(s[i]) != temp->children.end())
+                temp = temp->children[s[i]];
+            else 
+            {
+                Node* newNode = new Node(s[i]);
+                temp->children[s[i]] = newNode;
+                temp = newNode;
+            }
+        }
+        temp->terminal = true;
+    }
+    
+    bool search(string s) 
+    {
+        Node* temp = root;
+        for(int i=0;i < s.size();i++)
+        {
+            if(temp->children.find(s[i]) == temp->children.end())
+                return false;    
+            temp = temp->children[s[i]];
+        }
+        return temp->terminal;
+    }
+    
+    bool startsWith(string s) 
+    {
+        Node* temp = root;
+        for(int i=0;i<s.size();i++)
+        {
+            if(temp->children.find(s[i]) == temp->children.end())
+                return false;    
+            temp = temp->children[s[i]];
+        }
+        return true;
+    }
+};
+
+/**
+ * Your Trie object will be instantiated and called as such:
+ * Trie* obj = new Trie();
+ * obj->insert(word);
+ * bool param_2 = obj->search(word);
+ * bool param_3 = obj->startsWith(prefix);
+ */
