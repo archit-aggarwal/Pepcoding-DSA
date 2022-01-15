2e80fa7894a59fdc2322d8b8a0c69e8e07a1c493
Archit Aggarwal <archit.aggarwal023@gmail.com>
2022-01-15 17:48:00 +0530

    Update 212. Word Search - II.java

--- Level 2/01. Recursion & Backtracking/212. Word Search - II.java
+++ Level 2/01. Recursion & Backtracking/212. Word Search - II.java
@@ -1,24 +1,20 @@
-// This solution will give TLE. Implement Trie (Prefix Tree) for ACCEPTED verdict.
 class Solution {
     int[] x = {-1, 1, 0, 0};
     int[] y = {0, 0, -1, 1};
-    HashSet<String> dict;
     List<String> res;
-    int maxLengthWord = 0;
     
-    public void exist(int r, int c,  char[][] board, String ssof){
-        if(ssof.length() > maxLengthWord) return;
-        
-        if(dict.contains(ssof)) 
-        {
-            dict.remove(ssof);
-            res.add(ssof);
+    public void exist(int r, int c,  char[][] board, String ssof){    
+        if(){
+            // if word exists in dictionary or not
         }
         
         if(r < 0 || c < 0 || r >= board.length || c >= board[0].length 
            || board[r][c] == '0')
             return;
         
+        if(){
+            // if word is a prefix of another word or not    
+        }
         
         for(int call=0; call<4; call++){
             char ch = board[r][c];
@@ -30,12 +26,9 @@ class Solution {
     
     public List<String> findWords(char[][] board, String[] words) {
         res = new ArrayList<>();
-        dict = new HashSet<>();
         
-        maxLengthWord = 0;
         for(String word: words){
-            maxLengthWord = Math.max(word.length(), maxLengthWord);
-            dict.add(word);
+            // insert in trie
         }
             
         for(int i=0; i<board.length; i++){
