1365250b2781b00641b42a88bdf046c9ef845970
Archit Aggarwal <archit.aggarwal023@gmail.com>
2022-01-16 11:05:44 +0530

    Update Trie.java

--- Level 2/Trie.java
+++ Level 2/Trie.java
@@ -613,6 +613,87 @@ class Solution {
     }
 }
 
+// Leetcode 676
+class MagicDictionary {
+    public static class Node{
+        private Node[] children = new Node[26];
+        private boolean isTerminal = false;
+        
+        public Node get(char ch){
+            return children[ch - 'a'];
+        }
+        
+        public boolean isTerminal(){
+            return isTerminal;    
+        }
+        
+        public void setTerminal(){
+            isTerminal = true;
+        }
+        
+        public void add(char ch){
+            children[ch - 'a'] = new Node();
+        }
+        
+        public boolean contains(char ch){
+            return (children[ch - 'a'] != null);
+        }
+    }
+
+    Node root;
+    public MagicDictionary() {
+        root = new Node();
+    }
+    
+    public void buildDict(String[] dictionary) {
+        for(String word: dictionary){
+            Node curr = root;
+            for(int i=0; i<word.length(); i++){
+                char ch = word.charAt(i);
+                
+                if(curr.contains(ch) == false)
+                    curr.add(ch);
+                
+                curr = curr.get(ch);
+            }
+            curr.setTerminal();
+        }
+    }
+    
+    public boolean search(String word, int idx, Node curr, boolean change){
+        if(idx == word.length()){
+            if(change == true && curr.isTerminal() == true) return true;
+            return false;
+        }
+        
+        char ch = word.charAt(idx);
+        if(curr.contains(ch) == true && search(word, idx + 1, curr.get(ch), change))
+            return true;
+        
+        if(change == true) return false;
+        
+        for(char chn = 'a'; chn <= 'z'; chn++){
+            if(chn == ch) continue;
+            
+            if(curr.contains(chn) && search(word, idx + 1, curr.get(chn), true)){
+                return true;
+            }
+        }
+        return false;
+    }
+    
+    public boolean search(String searchWord) {
+        return search(searchWord, 0, root, false);
+    }
+}
+
+/**
+ * Your MagicDictionary object will be instantiated and called as such:
+ * MagicDictionary obj = new MagicDictionary();
+ * obj.buildDict(dictionary);
+ * boolean param_2 = obj.search(searchWord);
+ */
+
 // Leetcode 720
 class Solution {
     public static class Node{
@@ -678,7 +759,7 @@ class Solution {
     }
 }
 
-// Leetcode 
+// Leetcode 1268
 class Solution {
     public static class Node{
         private Node[] children = new Node[26];
