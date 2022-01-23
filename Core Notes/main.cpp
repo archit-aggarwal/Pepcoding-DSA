31ea76e8ccd50ccea58110317e876829ab279820
Archit Aggarwal <archit.aggarwal023@gmail.com>
2022-01-23 14:11:36 +0530

    Add files via upload

---
+++ Core Notes/main.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void file_input_output()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+#ifndef ONLINE_JUDGE
+	freopen("input.txt", "r", stdin);
+	freopen("output.txt", "w", stdout);
+#endif
+}
+
+int main() {
+	file_input_output();
+	int t;
+	cin >> t;
+	while (t--) {
+	}
+	return 0;
+}
