902cd152b1c2a4fc315825ebe16c5611c3eef8a4
Archit Aggarwal <archit.aggarwal023@gmail.com>
2022-02-11 16:34:58 +0530

    Create Q1.java

---
+++ Level 1/07. Stack & Queue/Contest/Q1.java
@@ -0,0 +1,110 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+class Result {
+
+    /*
+     * Complete the 'equalStacks' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts following parameters:
+     *  1. INTEGER_ARRAY h1
+     *  2. INTEGER_ARRAY h2
+     *  3. INTEGER_ARRAY h3
+     */
+
+    public static int equalStacks(List<Integer> h1, List<Integer> h2, List<Integer> h3) {
+        int sum1 = 0, sum2 = 0, sum3 = 0;
+        for(int i=0;i<h1.size();i++){
+            sum1 += h1.get(i);
+        }
+        
+        for(int i=0;i<h2.size();i++){
+            sum2 += h2.get(i);
+        }
+        
+        for(int i=0;i<h3.size();i++){
+            sum3 += h3.get(i);
+        }
+        
+        int minsum = Math.min(sum1,sum2);
+        minsum = Math.min(minsum, sum3);
+        
+        while(sum1 != sum2 || sum2!=sum3){
+            while(sum1>minsum){
+                sum1 -= h1.remove(0);
+            }
+            
+            minsum = Math.min(minsum, sum1);
+            while(sum2>minsum){
+                sum2 -= h2.remove(0);
+            }
+            
+            minsum = Math.min(minsum, sum2);
+            
+            while(sum3>minsum){
+                sum3 -= h3.remove(0);
+            }
+            
+            minsum = Math.min(minsum, sum3);
+        }
+        
+        return sum1;
+    }
+
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        String[] firstMultipleInput = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
+
+        int n1 = Integer.parseInt(firstMultipleInput[0]);
+
+        int n2 = Integer.parseInt(firstMultipleInput[1]);
+
+        int n3 = Integer.parseInt(firstMultipleInput[2]);
+
+        String[] h1Temp = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
+
+        List<Integer> h1 = new ArrayList<>();
+
+        for (int i = 0; i < n1; i++) {
+            int h1Item = Integer.parseInt(h1Temp[i]);
+            h1.add(h1Item);
+        }
+
+        String[] h2Temp = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
+
+        List<Integer> h2 = new ArrayList<>();
+
+        for (int i = 0; i < n2; i++) {
+            int h2Item = Integer.parseInt(h2Temp[i]);
+            h2.add(h2Item);
+        }
+
+        String[] h3Temp = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
+
+        List<Integer> h3 = new ArrayList<>();
+
+        for (int i = 0; i < n3; i++) {
+            int h3Item = Integer.parseInt(h3Temp[i]);
+            h3.add(h3Item);
+        }
+
+        int result = Result.equalStacks(h1, h2, h3);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
