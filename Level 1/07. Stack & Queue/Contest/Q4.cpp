1c7f3baf0fb13e02f003dbce27bf3745020b7be3
Archit Aggarwal <archit.aggarwal023@gmail.com>
2022-02-11 16:36:04 +0530

    Create Q4.cpp

---
+++ Level 1/07. Stack & Queue/Contest/Q4.cpp
@@ -0,0 +1,108 @@
+#include <vector>
+#include <list>
+#include <map>
+#include <set>
+#include <queue>
+#include <deque>
+#include <stack>
+#include <bitset>
+#include <algorithm>
+#include <functional>
+#include <numeric>
+#include <limits>
+#include <tuple>
+#include <utility>
+#include <sstream>
+#include <iostream>
+#include <iomanip>
+#include <cstdio>
+#include <cmath>
+#include <cstdlib>
+#include <ctime>
+#include <cassert>
+
+using namespace std;
+
+typedef struct query
+{
+    int type, remove;
+    string add;
+}query;
+
+char stak[1000003];
+
+int topp = 0;
+
+void push(string s)
+{
+    for(int i = 0 ; i < s.length(); i++)
+    {
+        stak[++topp] = s[i];
+    }
+    assert(topp <= 1000000);
+}
+
+string pop(int remove)
+{
+    assert(remove >= 0 && remove <= topp);
+    string popped = "";
+    int del = remove;
+    for (int  i = topp; del > 0; i--, del--) {
+        popped = stak[i] + popped;
+    }
+    topp -= remove;
+    return popped;
+}
+
+int main()
+{
+    int t, type, remove, k;
+    stack<query> q_stack;
+    cin>>t;
+    while(t--)
+    {
+        cin>>type;
+        if(type == 1)
+        {
+            string add;
+            cin>>add;
+            push(add);
+            query last;
+            last.type = type;
+            last.add = add;
+            q_stack.push(last);
+        }
+        else if(type == 2)
+        {
+            cin>>remove;
+            string popped = pop(remove);
+            query last;
+            last.type = type;
+            last.remove = remove;
+            last.add = popped;
+            q_stack.push(last);
+        }
+        else if(type == 3)
+        {
+            cin>>k;
+            assert(k >= 1 && k <= topp);
+            cout<<stak[k]<<endl;
+        }
+        else
+        {
+            query last = q_stack.top();
+            q_stack.pop();
+            if(last.type == 1)
+            {
+                int remove = (last.add).length();
+                string popped = pop(remove);
+            }
+            else
+            {
+                push(last.add);
+                assert(topp >= 1 && topp <= 1000000);
+            }
+        }
+    }
+    return 0;
+}
