2104f63c624e0fd6e2e422a50d2a04d96122eee0
Archit Aggarwal <archit.aggarwal023@gmail.com>
2022-02-12 17:43:00 +0530

    Time: 0 ms (100.00%), Space: 43.5 MB (10.43%) - LeetHub

--- 543-diameter-of-binary-tree/543-diameter-of-binary-tree.java
+++ 543-diameter-of-binary-tree/543-diameter-of-binary-tree.java
@@ -14,24 +14,38 @@
  * }
  */
 class Solution {
-    public int height(TreeNode root){
-        if(root == null) return 0;
-        return 1 + Math.max(height(root.left), height(root.right));
-    }
+//     public int height(TreeNode root){
+//         if(root == null) return 0;
+//         return 1 + Math.max(height(root.left), height(root.right));
+//     }
     
-    public int diameter(TreeNode root) {
+//     public int diameter(TreeNode root) {
+//         if(root == null) return 0;
+        
+//         int lh = height(root.left);
+//         int rh = height(root.right);
+        
+//         int ld = diameter(root.left);
+//         int rd = diameter(root.right);
+//         return Math.max(lh + rh + 1, Math.max(ld, rd));
+//     }
+    
+    public int globalDia = 0;
+    public int diameter(TreeNode root){
         if(root == null) return 0;
         
-        int lh = height(root.left);
-        int rh = height(root.right);
+        int lh = diameter(root.left);
+        int rh = diameter(root.right);
         
-        int ld = diameter(root.left);
-        int rd = diameter(root.right);
-        return Math.max(lh + rh + 1, Math.max(ld, rd));
+        // Global Variable Strategy or Travel & Change Strategy
+        globalDia = Math.max(globalDia, lh + rh + 1); 
+        return Math.max(lh, rh) + 1;
     }
     
     public int diameterOfBinaryTree(TreeNode root){
         if(root == null) return 0;
-        return diameter(root) - 1;
+        diameter(root);
+        return globalDia - 1;
+        // Diameter in Terms of Edges = Diamater in Terms of Nodes - 1
     }
 }
