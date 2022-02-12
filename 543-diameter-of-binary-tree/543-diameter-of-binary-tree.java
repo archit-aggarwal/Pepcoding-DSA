183d5e5b513b0fcf033114d0f2656fe30b9101a1
Archit Aggarwal <archit.aggarwal023@gmail.com>
2022-02-12 17:55:19 +0530

    Time: 1 ms (53.28%), Space: 43.4 MB (10.43%) - LeetHub

--- 543-diameter-of-binary-tree/543-diameter-of-binary-tree.java
+++ 543-diameter-of-binary-tree/543-diameter-of-binary-tree.java
@@ -30,22 +30,24 @@ class Solution {
 //         return Math.max(lh + rh + 1, Math.max(ld, rd));
 //     }
     
-    public int globalDia = 0;
-    public int diameter(TreeNode root){
+   
+    public int diameter(TreeNode root, int[] globalDia){
         if(root == null) return 0;
         
-        int lh = diameter(root.left);
-        int rh = diameter(root.right);
+        int lh = diameter(root.left, globalDia);
+        int rh = diameter(root.right, globalDia);
         
         // Global Variable Strategy or Travel & Change Strategy
-        globalDia = Math.max(globalDia, lh + rh + 1); 
+        globalDia[0] = Math.max(globalDia[0], lh + rh + 1); 
         return Math.max(lh, rh) + 1;
     }
     
     public int diameterOfBinaryTree(TreeNode root){
         if(root == null) return 0;
-        diameter(root);
-        return globalDia - 1;
+        int[] globalDia = new int[1];
+        
+        diameter(root, globalDia);
+        return globalDia[0] - 1;
         // Diameter in Terms of Edges = Diamater in Terms of Nodes - 1
     }
 }
