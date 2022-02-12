2c3d7986cc595c03ca4d2de6ec7535d4871fcb45
Archit Aggarwal <archit.aggarwal023@gmail.com>
2022-02-12 18:00:05 +0530

    Time: 1 ms (53.28%), Space: 43.8 MB (5.75%) - LeetHub

--- 543-diameter-of-binary-tree/543-diameter-of-binary-tree.java
+++ 543-diameter-of-binary-tree/543-diameter-of-binary-tree.java
@@ -30,24 +30,38 @@ class Solution {
 //         return Math.max(lh + rh + 1, Math.max(ld, rd));
 //     }
     
-   
-    public int diameter(TreeNode root, int[] globalDia){
-        if(root == null) return 0;
+//     public int diameter(TreeNode root, int[] globalDia){
+//         if(root == null) return 0;
         
-        int lh = diameter(root.left, globalDia);
-        int rh = diameter(root.right, globalDia);
+//         int lh = diameter(root.left, globalDia);
+//         int rh = diameter(root.right, globalDia);
         
-        // Global Variable Strategy or Travel & Change Strategy
-        globalDia[0] = Math.max(globalDia[0], lh + rh + 1); 
-        return Math.max(lh, rh) + 1;
+//         // Global Variable Strategy or Travel & Change Strategy
+//         globalDia[0] = Math.max(globalDia[0], lh + rh + 1); 
+//         return Math.max(lh, rh) + 1;
+//     }
+    
+    public static class Pair{
+        int height;
+        int diameter; // Diameter is of entire subtree
+    }
+    
+    public Pair diameter(TreeNode root){
+        if(root == null) return new Pair();
+        
+        Pair left = diameter(root.left);
+        Pair right = diameter(root.right);
+        
+        Pair curr = new Pair();
+        curr.height = Math.max(left.height, right.height) + 1;
+        curr.diameter = left.height + right.height + 1;
+        curr.diameter = Math.max(curr.diameter, Math.max(left.diameter, right.diameter));
+        return curr;
     }
     
     public int diameterOfBinaryTree(TreeNode root){
         if(root == null) return 0;
-        int[] globalDia = new int[1];
-        
-        diameter(root, globalDia);
-        return globalDia[0] - 1;
         // Diameter in Terms of Edges = Diamater in Terms of Nodes - 1
+        return diameter(root).diameter - 1;
     }
 }
