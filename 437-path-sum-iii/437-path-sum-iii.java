9825d46647dca4c58b5fb56b9b79344ada4227b8
Archit Aggarwal <archit.aggarwal023@gmail.com>
2022-02-13 22:47:29 +0530

    Time: 4 ms (79.54%), Space: 44.4 MB (13.94%) - LeetHub

--- 437-path-sum-iii/437-path-sum-iii.java
+++ 437-path-sum-iii/437-path-sum-iii.java
@@ -14,22 +14,50 @@
  * }
  */
 
-class Solution {
-    public int dfs(TreeNode root, int targetSum){
+// O(N ^ 2) Approach
+// class Solution {
+    
+//     public int dfs(TreeNode root, int targetSum){
+//         if(root == null) return 0;
+        
+//         targetSum -= root.val;
+        
+//         int count = 0;
+//         if(targetSum == 0)
+//             count++;
+        
+//         return count + dfs(root.left, targetSum) + dfs(root.right, targetSum);
+//     }
+    
+//     public int pathSum(TreeNode root, int targetSum) {
+//         if(root == null) return 0;
+//         return dfs(root, targetSum) + pathSum(root.left, targetSum) + 
+//             pathSum(root.right, targetSum);
+//     }
+// }
+
+// O(N) Approach
+class Solution{
+    HashMap<Integer, Integer> freq = new HashMap<>();
+    
+    public int helper(TreeNode root, int targetSum, int prefSum){
         if(root == null) return 0;
         
-        targetSum -= root.val;
+        prefSum += root.val;
+        int count = freq.getOrDefault(prefSum - targetSum, 0);
+        freq.put(prefSum, freq.getOrDefault(prefSum, 0) + 1);
         
-        int count = 0;
-        if(targetSum == 0)
-            count++;
+        count += helper(root.left, targetSum, prefSum);
+        count += helper(root.right, targetSum, prefSum);
         
-        return count + dfs(root.left, targetSum) + dfs(root.right, targetSum);
+        freq.put(prefSum, freq.getOrDefault(prefSum, 0) - 1); // Backtrack
+        
+        return count;
     }
     
-    public int pathSum(TreeNode root, int targetSum) {
+    public int pathSum(TreeNode root, int targetSum){
         if(root == null) return 0;
-        return dfs(root, targetSum) + pathSum(root.left, targetSum) + 
-            pathSum(root.right, targetSum);
+        freq.put(0, 1);
+        return helper(root, targetSum, 0);
     }
 }
