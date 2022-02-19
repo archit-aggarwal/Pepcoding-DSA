efe829fbaf004f541d2765b8e783c36c30fec9ee
Archit Aggarwal <archit.aggarwal023@gmail.com>
2022-02-19 17:58:51 +0530

    Added solution - LeetHub

--- Left View of Binary Tree - GFG/left-view-of-binary-tree.java
+++ Left View of Binary Tree - GFG/left-view-of-binary-tree.java
@@ -121,26 +121,20 @@ class Node
 }*/
 class Tree
 {
-    //Function to return list containing elements of left view of binary tree.
+    public void DFS(Node root, ArrayList<Integer> leftView, int level){
+        if(root == null) return;
+        
+        if(level >= leftView.size())
+            leftView.add(root.data);
+        
+        DFS(root.left, leftView, level + 1);
+        DFS(root.right, leftView, level + 1);
+    }
+    
     ArrayList<Integer> leftView(Node root)
     {
         ArrayList<Integer> leftView = new ArrayList<>();
-        if(root == null) return leftView;
-        
-        Queue<Node> q = new ArrayDeque<>();
-        q.add(root);
-        
-        while(q.size() > 0){
-            int count = q.size();
-            for(int i=0; i<count; i++){
-                Node curr = q.remove();
-                
-                if(i == 0) leftView.add(curr.data);
-                if(curr.left != null) q.add(curr.left);
-                if(curr.right != null) q.add(curr.right);
-            }
-        }
-        
+        DFS(root, leftView, 0);
         return leftView;
     }
 }
