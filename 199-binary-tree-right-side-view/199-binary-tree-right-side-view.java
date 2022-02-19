dcda153bac2261643b8eeb5a2780e6bb4c9df730
Archit Aggarwal <archit.aggarwal023@gmail.com>
2022-02-19 17:56:31 +0530

    Time: 2 ms (42.26%), Space: 42.7 MB (20.88%) - LeetHub

--- 199-binary-tree-right-side-view/199-binary-tree-right-side-view.java
+++ 199-binary-tree-right-side-view/199-binary-tree-right-side-view.java
@@ -15,24 +15,20 @@
  */
 
 class Solution {
+    public void DFS(TreeNode root, List<Integer> rightView, int level){
+        if(root == null) return;
+        
+        if(level >= rightView.size())
+            rightView.add(root.val);
+        else rightView.set(level, root.val);
+        
+        DFS(root.left, rightView, level + 1);
+        DFS(root.right, rightView, level + 1);
+    }
+    
     public List<Integer> rightSideView(TreeNode root) {
         List<Integer> rightView = new ArrayList<>();
-        if(root == null) return rightView;
-        
-        Queue<TreeNode> q = new ArrayDeque<>();
-        q.add(root);
-        
-        while(q.size() > 0){
-            int count = q.size();
-            for(int i=0; i<count; i++){
-                TreeNode curr = q.remove();
-                
-                if(i == count - 1) rightView.add(curr.val);
-                if(curr.left != null) q.add(curr.left);
-                if(curr.right != null) q.add(curr.right);
-            }
-        }
-        
+        DFS(root, rightView, 0);
         return rightView;
     }
 }
