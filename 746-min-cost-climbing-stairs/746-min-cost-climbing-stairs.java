74a9a4969c625a185f238f0a5f92d259e8d28170
Archit Aggarwal <archit.aggarwal023@gmail.com>
2022-04-23 16:22:26 +0530

    Time: 1 ms (82.39%), Space: 41.7 MB (93.41%) - LeetHub

---
+++ 746-min-cost-climbing-stairs/746-min-cost-climbing-stairs.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int memo(int src, int[] cost, int[] dp){
+        if(src >= cost.length) return 0;
+        if(dp[src] != -1) return dp[src];
+        
+        int ans1 = memo(src + 1, cost, dp);
+        int ans2 = memo(src + 2, cost, dp);
+        
+        return dp[src] = Math.min(ans1, ans2) + cost[src];
+    }
+    
+    public int minCostClimbingStairs(int[] cost) {
+        int[] dp = new int[cost.length + 1];
+        Arrays.fill(dp, -1);
+        
+        memo(0, cost, dp);
+        return Math.min(dp[0], dp[1]);
+    }
+}
