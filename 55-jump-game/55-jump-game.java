852ede2164917bbf1d9271b54c3c4877025066b6
Archit Aggarwal <archit.aggarwal023@gmail.com>
2022-04-23 15:36:24 +0530

    Time: 2 ms (86.04%), Space: 42.9 MB (81.66%) - LeetHub

--- 55-jump-game/55-jump-game.java
+++ 55-jump-game/55-jump-game.java
@@ -1,11 +1,11 @@
 class Solution {
-    public boolean canJump(int[] nums) {
-        int reachable = 0;
-        for(int i=0; i<nums.length; i++){
-            if(i > reachable) return false;
+    public boolean canJump(int[] jumps) {
+        int reach = 0;
+        for(int i=0; i<jumps.length; i++){
+            if(i > reach) return false;
             
-            if(nums[i] + i >= reachable){
-                reachable = nums[i] + i;
+            if(i + jumps[i] > reach){
+                reach = i + jumps[i];
             }
         }
         
