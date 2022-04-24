583e50b8faa14c460e50523b4e9dc450a64b09f7
Archit Aggarwal <archit.aggarwal023@gmail.com>
2022-04-24 11:49:26 +0530

    Added solution - LeetHub

--- Count the number of ways to tile the floor of size n x m using 1 x m size tiles - GFG/count-the-number-of-ways-to-tile-the-floor-of-size-n-x-m-using-1-x-m-size-tiles.java
+++ Count the number of ways to tile the floor of size n x m using 1 x m size tiles - GFG/count-the-number-of-ways-to-tile-the-floor-of-size-n-x-m-using-1-x-m-size-tiles.java
@@ -30,20 +30,36 @@ class GFG
 class Solution
 {
     int mod = 1000000007;
-    public int memo(int n, int m, int[] dp){
-        if(n < m) return 1;
-        if(n == m) return 2;
-        if(dp[n] != -1) return dp[n];
+    // public int memo(int n, int m, int[] dp){
+    //     if(n < m) return 1;
+    //     if(n == m) return 2;
+    //     if(dp[n] != -1) return dp[n];
         
-        int ans1 = memo(n - 1, m, dp);
-        int ans2 = memo(n - m, m, dp);
-        return dp[n] = (ans1 + ans2) % mod;
-    }
+    //     int ans1 = memo(n - 1, m, dp);
+    //     int ans2 = memo(n - m, m, dp);
+    //     return dp[n] = (ans1 + ans2) % mod;
+    // }
     
     public int countWays(int n, int m)
     {
-        int[] dp = new int[n + 1];    
-        Arrays.fill(dp, -1);
-        return memo(n, m, dp);
+        if(n < m) return 1;
+        if(n == m) return 2;
+        // int[] dp = new int[n + 1];    
+        // Arrays.fill(dp, -1);
+        // return memo(n, m, dp);
+        
+        Deque<Integer> dp = new ArrayDeque<>();
+        for(int i=1; i<m; i++){
+            dp.add(1); // DP[N < M]
+        }
+        dp.add(2); // DP[N == M]
+        
+        for(int i=m+1; i<=n; i++){
+            int ans = (dp.getFirst() + dp.getLast()) % mod;
+            dp.removeFirst();
+            dp.addLast(ans);
+        }
+        
+        return dp.getLast();
     }
 }
