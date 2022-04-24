a0b623cd541999a15766bada3f1c664bb1e7ba2f
Archit Aggarwal <archit.aggarwal023@gmail.com>
2022-04-24 10:44:56 +0530

    Time: 1 ms (94.75%), Space: 43 MB (15.50%) - LeetHub

--- 91-decode-ways/91-decode-ways.java
+++ 91-decode-ways/91-decode-ways.java
@@ -1,26 +1,27 @@
 class Solution {
-    public int numDecodings(String str) {
-        if(str.length() == 0 || str.charAt(0) == '0')
-            return 0;
+    public int memo(String s, int idx, int[] dp){
+        if(idx == s.length()) return 1;
+        if(dp[idx] != -1) return dp[idx];
         
-        int[] dp = new int[str.length()];
-        dp[0] = 1;
-        for (int i = 1; i < dp.length; i++) {
-          if (str.charAt(i - 1) == '0') {
-            if (str.charAt(i) != '0') {
-              dp[i] = dp[i - 1];
-            }
-          } else {
-            if (str.charAt(i) != '0') {
-              dp[i] = dp[i - 1];
-            }
-
-            if (Integer.parseInt(str.substring(i - 1, i + 1)) <= 26) {
-              dp[i] += i == 1 ? 1 : dp[i - 2];
-            }
-          }
+        int ans1 = 0, ans2 = 0;
+        if(s.charAt(idx) != '0'){
+            ans1 = memo(s, idx + 1, dp);
         }
-
-        return dp[str.length() - 1];
+        
+        
+        if(idx + 1 < s.length() && s.charAt(idx) != '0'){
+            int code = (s.charAt(idx) - '0') * 10 + (s.charAt(idx + 1) - '0');
+            if(code >= 10 && code <= 26){
+                ans2 = memo(s, idx + 2, dp);
+            }
+        }
+        
+        return dp[idx] = ans1 + ans2;
+    }
+    
+    public int numDecodings(String s) {
+        int[] dp = new int[s.length() + 1];
+        Arrays.fill(dp, -1);
+        return memo(s, 0, dp);
     }
 }
