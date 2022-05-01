890c12f89f288be5d32127a3ba1662c613095052
Archit Aggarwal <archit.aggarwal023@gmail.com>
2022-05-01 11:36:43 +0530

    Time: 18 ms (81.53%), Space: 42 MB (84.46%) - LeetHub

--- 322-coin-change/322-coin-change.java
+++ 322-coin-change/322-coin-change.java
@@ -1,29 +1,16 @@
 class Solution {
-    // Time Complexity: O(Amount * Coins), Space Complexity: O(Amount * Coins)
-    public int memo(int amount, int idx, int[] coins, int[][] dp){
-        if(amount < 0) return Integer.MAX_VALUE;
-        if(amount == 0) return 0;
-        if(idx == coins.length) return Integer.MAX_VALUE;
-        if(dp[amount][idx] != -1) return dp[amount][idx];
-        
-        int yes = memo(amount - coins[idx], idx, coins, dp);
-        if(yes != Integer.MAX_VALUE) yes += 1;
-        
-        int no = memo(amount, idx + 1, coins, dp);
-        
-        return dp[amount][idx] = Math.min(yes, no);
-    }
-    
     public int coinChange(int[] coins, int amount) {
-        int[][] dp = new int[amount + 1][coins.length];
-        for(int i=0; i<=amount; i++){
-            for(int j=0; j<coins.length; j++){
-                dp[i][j] = -1;
+        int[] dp = new int[amount + 1];
+        Arrays.fill(dp, Integer.MAX_VALUE);
+        dp[0] = 0;
+        
+        for(int i=1; i<=amount; i++){
+            for(int coin: coins){
+                if(i - coin >= 0 && dp[i - coin] != Integer.MAX_VALUE) 
+                    dp[i] = Math.min(dp[i], dp[i - coin] + 1);
             }
         }
         
-        int ans = memo(amount, 0, coins, dp);
-        if(ans == Integer.MAX_VALUE) return -1;
-        return ans;
+        return (dp[amount] == Integer.MAX_VALUE) ? -1 : dp[amount];
     }
 }
