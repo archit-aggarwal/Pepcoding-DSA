7e2e4f0d177a082d77a7b6bd77fb47a6a4d20b5c
Archit Aggarwal <archit.aggarwal023@gmail.com>
2022-05-03 21:39:10 +0530

    Time: 5 ms (63.83%), Space: 42 MB (63.30%) - LeetHub

--- 518-coin-change-2/518-coin-change-2.java
+++ 518-coin-change-2/518-coin-change-2.java
@@ -1,26 +1,16 @@
 class Solution {
-    // Time - O(Amount * Coins), Space - O(Amount * Coins): 2D DP + RCS
-    public int memo(int amount, int lastCoin, int[] coins, int[][] dp) {
-        if (amount < 0) return 0;
-        if (amount == 0) return 1;
-        if (dp[amount][lastCoin] != -1) return dp[amount][lastCoin];
-
-        int perm = 0;
-        for (int coin = lastCoin; coin < coins.length; coin++) {
-                perm += memo(amount - coins[coin], coin, coins, dp);
-        }
-
-        return dp[amount][lastCoin] = perm;
-    }
-    
     public int change(int amount, int[] coins) {
-        int[][] dp = new int[amount + 1][coins.length + 1];
-        for(int i=0; i<=amount; i++){
-            for(int j=0; j<=coins.length; j++){
-                dp[i][j] = -1;
+        int[] dp = new int[amount + 1];
+        dp[0] = 1; // Ways to reach dest when src = dest is 1. (Empty String)
+        
+        // Time - O(Amount * Coins), Space - O(Amount)
+        for(int coin: coins){
+            for(int i=1; i<= amount; i++){
+                if(i >= coin){
+                    dp[i] += dp[i - coin];
+                }
             }
         }
-        
-        return memo(amount, 0, coins, dp);
+        return dp[amount];
     }
 }
