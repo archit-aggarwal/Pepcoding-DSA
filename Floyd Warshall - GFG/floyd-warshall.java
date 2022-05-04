0b331acf7161cd55cd801e7740b8d0f655a96f18
Archit Aggarwal <archit.aggarwal023@gmail.com>
2022-05-04 11:25:53 +0530

    Added solution - LeetHub

--- Floyd Warshall - GFG/floyd-warshall.java
+++ Floyd Warshall - GFG/floyd-warshall.java
@@ -43,13 +43,11 @@ class Solution
         for(int mid=0; mid<n; mid++){
             for(int src=0; src<n; src++){
                 for(int dest=0; dest<n; dest++){
-                    if(src != mid && dest != mid && 
-                        mat[src][mid] != -1 && mat[mid][dest] != -1){
-                        if(mat[src][dest] == -1) 
-                            mat[src][dest] = Integer.MAX_VALUE;
-                        mat[src][dest] = Math.min(mat[src][dest], 
-                        mat[src][mid] + mat[mid][dest]);        
-                    }
+                    if(src == mid || dest == mid || mat[src][mid] == -1 || mat[mid][dest] == -1)
+                        continue;
+                        
+                    if(mat[src][dest] == -1) mat[src][dest] = Integer.MAX_VALUE;
+                    mat[src][dest] = Math.min(mat[src][dest], mat[src][mid] + mat[mid][dest]);        
                 }
             }
         }
